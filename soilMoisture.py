140a1af05d1588ce3f26f382c520816691ab3221
Sydney <sydney.vieveen6412@gmail.com>
2025-02-10 12:21:11 -0500

    Made the soilmoisture into a class.
    Signed-off-by: Sydney <sydney.vieveen6412@gmail.com>

--- soilMoisture.py
+++ soilMoisture.py
@@ -2,22 +2,22 @@
 # SPDX-License-Identifier: MIT
 
 import time
-
 import board
-
 from adafruit_seesaw.seesaw import Seesaw
 
-i2c_bus = board.I2C()  # uses board.SCL and board.SDA
-# i2c_bus = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
-ss = Seesaw(i2c_bus, addr=0x36)
+class soilMoisture:
+    #Can print the temperature and moisture
+    
+    # constructor for the soil moisture class
+    def __init__(self):
+        i2c_bus = board.I2C()  # uses board.SCL and board.SDA
+        self.ss = Seesaw(i2c_bus, addr=0x36)
 
-while True:
-    # read moisture level through capacitive touch pad
-    touch = ss.moisture_read()
+    # gets the moisture level through capacitive touch pad
+    def get_moist(self):
+        return self.ss.moisture_read()
 
-    # read temperature from the temperature sensor
-    temp = ss.get_temp()
-
-    print("temp: " + str(temp) + "  moisture: " + str(touch))
-    time.sleep(1)
+    # gets the temperature in degrees C
+    def get_temp(self):
+        return self.ss.get_temp()
