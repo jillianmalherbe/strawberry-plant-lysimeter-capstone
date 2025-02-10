f403f1281ba8fe34cfa615dbafff92a30399fb61
Sydney <sydney.vieveen6412@gmail.com>
2025-02-10 11:22:26 -0500

    Made the Co2 file into a class
    Signed-off-by: Sydney <sydney.vieveen6412@gmail.com>

--- CO2etal.py
+++ CO2etal.py
@@ -1,9 +1,27 @@
 import board
 import adafruit_scd30
 
-scd = adafruit_scd30.SCD30(board.I2C())
+class CO2:
+	#Can print the c02 levels, temperature, and humidity
+	
+	# constructor for the co2 class
+	def __init__(self):
+		self.scd = adafruit_scd30.SCD30(board.I2C())
+		print(self.scd)
 
-print("Data available?", scd.data_available)
-print("CO2:", scd.CO2, "PPM")
-print("Temperature:", scd.temperature, "degrees C")
-print("Humidity:", scd.relative_humidity, "%%rH")
+	# gets if the data is available to read
+	def get_data_available(self):
+		return self.scd.data_available
+
+	# gets the co2 in ppm
+	def get_co2(self):
+		return self.scd.CO2
+
+	# gets the temperature in degrees C
+	def get_temp(self):
+		return self.scd.temperature
+
+	# gets the humidity in rH
+	def get_humid(self):
+		return self.scd.relative_humidity
+     
